f329427c60b66a183b38812a934f3fbdd5f15651
David CLEMENT <dclement@dawan.fr>
2026-09-18 17:12:52 +0200

    Dépôt de la formation

---
+++ 1-intro.ps1
@@ -0,0 +1,9 @@
+# Notes:
+# https://hedgedoc.dawan.fr/s/HmiNJQFBs
+
+# Github :
+# https://github.com/dclement-dawan/powershell09-26
+# https://github.com/dclement-dawan/powershell09-26.git
+
+# OneDrive :
+# https://dawaneducation-my.sharepoint.com/:f:/g/personal/dclement_dawan_education/IgC2Rt26Iab0SqmNwjtE-cnuAQGd7ulEcZnv3HaP2QcDCXc?e=0zvXuI
