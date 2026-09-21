26893e826a48365ef51d1b00f36d188a75a397e1
David CLEMENT <dclement@dawan.fr>
2026-09-21 16:55:25 +0200

    Correction tp 6

--- 6-tp.ps1
+++ 6-tp.ps1
@@ -17,3 +17,26 @@ En fonction du choix : (switch)
     - autre choix : afficher un avertissement 'Mauvais choix'
     (Write-Warning)
 #>
+Clear-Host
+Write-Host "====================="
+Write-Host "1 - Créer un fichier"
+Write-Host "2 - Créer un dossier"
+Write-Host "3 - Afficher le contenu du dossier courant"
+Write-Host "4 - Afficher le contenu d'un fichier"
+$choix = Read-Host "Votre choix de 1 à 4"
+switch ($choix) {
+    1 {
+        $fichier = Read-Host "Fichier à créer"
+        New-Item $fichier
+    }
+    2 {
+        $dossier = Read-Host "Dossier à créer"
+        New-Item $dossier -ItemType Directory
+    }
+    3 { Get-ChildItem }
+    4 {
+        $fichier = Read-Host "Fichier à afficher"
+        Get-Content $fichier
+    }
+    Default { Write-Warning "Mauvais choix. Choix valides 1 à 4" }
+}
