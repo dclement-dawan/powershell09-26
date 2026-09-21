41535bf90e074319fbe324cdfe7582776ba1fdfa
David CLEMENT <dclement@dawan.fr>
2026-09-21 11:42:13 +0200

    intro + tp

--- 1-intro.ps1
+++ 1-intro.ps1
@@ -7,3 +7,48 @@
 
 # OneDrive :
 # https://dawaneducation-my.sharepoint.com/:f:/g/personal/dclement_dawan_education/IgC2Rt26Iab0SqmNwjtE-cnuAQGd7ulEcZnv3HaP2QcDCXc?e=0zvXuI
+
+
+# ctrl + shift + p : preference
+
+# Alias:
+ls
+
+# Commandes externes:
+ping nas
+
+# Cmdlets:
+Get-ChildItem
+Test-Connection nas
+
+Get-Command *-process
+Get-Command *-service
+Get-Command *-localuser
+
+# Commandes intégrées, builtin, primitives : if, switch, foreach, for, while, try...
+if ($true) { 'Pause café ! :)' }
+
+
+man stop-service -Online
+
+
+
+
+# Les objets powershell
+
+# Résultat formaté
+Get-ChildItem
+Get-Process
+Get-Service
+
+# Formatage du résultat
+Get-ChildItem | Select *
+Get-ChildItem | Select * | Out-GridView # présentation graphique
+Get-ChildItem | Select * | ogv
+
+Get-ChildItem | Select Name, *time
+Get-ChildItem | Select-Object Name, *time
+Get-ChildItem | fl Name, ext*, *name*
+Get-ChildItem | Format-List Name, ext*, *name*
+Get-ChildItem | ft ext*, *name*
+Get-ChildItem | Format-Table Name, ext*, *name*
---
+++ 2-tp.ps1
@@ -0,0 +1,14 @@
+# 1- Démarrer le process Notepad et le terminer
+# Avec son nom puis son PID
+
+Get-Command
+Get-Help stop-process -Examples
+
+
+# 2- Créer un dossier "temp" dans l'emplacement courant
+
+# 3- Créer un fichier test.pdf dans le dossier temp
+
+# 4- Afficher les fichiers avec leur nom et extension
+
+# 5- Afficher les fichiers avec leur nom et les dates associées
