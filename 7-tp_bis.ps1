13da432fec08d98dfafe3272b1340ff59a73a349
David CLEMENT <dclement@dawan.fr>
2026-09-21 17:03:04 +0200

    tp 7

---
+++ 7-tp_bis.ps1
@@ -0,0 +1,38 @@
+<# - Ajouter un test sur les cas 1 et 2 pour valider que le fichier ou le dossier n'existe pas.
+s'il existe afficher un warning.
+
+- Ajouter un test sur le cas 4 pour valider que le fichier existe.
+s'il n'existe pas alors afficher un warning.
+
+-not(Test-Path test)
+Test-Path .\temp -PathType Leaf
+#>
+# $fichier= 'truc'
+# Test-Path $fichier
+# Test-Path temp -PathType Leaf
+
+# -not(Test-Path temp)
+
+Clear-Host
+Write-Host "====================="
+Write-Host "1 - Créer un fichier"
+Write-Host "2 - Créer un dossier"
+Write-Host "3 - Afficher le contenu du dossier courant"
+Write-Host "4 - Afficher le contenu d'un fichier"
+$choix = Read-Host "Votre choix de 1 à 4"
+switch ($choix) {
+    1 {
+        $fichier = Read-Host "Fichier à créer"
+        New-Item $fichier
+    }
+    2 {
+        $dossier = Read-Host "Dossier à créer"
+        New-Item $dossier -ItemType Directory
+    }
+    3 { Get-ChildItem }
+    4 {
+        $fichier = Read-Host "Fichier à afficher"
+        Get-Content $fichier
+    }
+    Default { Write-Warning "Mauvais choix. Choix valides 1 à 4" }
+}
