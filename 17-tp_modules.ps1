49413bee8edfadbc131ac7286769818b8f4ebcb7
David CLEMENT <dclement@dawan.fr>
2026-09-22 16:39:17 +0200

    Modiff TP

--- 17-tp_modules.ps1
+++ 17-tp_modules.ps1
@@ -7,27 +7,27 @@
 
 # Créer une fonction dans ce module Get-Manufacturer2
 
+# Modifier la v1 de la fonction Get-Manufacturer
 # Au début de la fonction :
+
+
+# Ajouter un paramètre de type [Switch]$DownloadOUI
+
+# Si $DownloadOUI est vrai , alors supprimer le fichier oui.txt
+
+#  Si le fichier oui.txt a plus 15 jours alors le supprimer
+#     (Get-Item ./oui.txt).CreationTime
 
 #  Vérifier la présence du fichier oui.txt (Test-Path), s'il n'existe pas le télécharger
 #    Invoke-WebRequest
 # https://standards-oui.ieee.org/  -> oui.txt
 
-
-#  Si le fichier oui.txt a plus 15 jours, le re-télécharger.
-#     (Get-Item ./oui.txt).CreationTime
-
-
-# Ajouter un paramètre de type [Switch] $DownloadOUI
-# Si $DownloadOUI est vrai , alors re-télécharger le fichier.
-
-
-
 # Modifier la date pour le test:
-(Get-Item ./oui.txt).CreationTime = '01/01/2026'
+(Get-Item .\oui.txt).CreationTime = '01/01/2026'
 
-(Get-Item ./oui.txt).CreationTime
+(Get-Item .\oui.txt).CreationTime
 
-((Get-Date) -  (Get-Item ./oui.txt).CreationTime ).TotalDays -gt 15
+# Tester les dates :
+((Get-Date) -  (Get-Item .\oui.txt).CreationTime ).TotalDays -gt 15
 
-(Get-Item ./oui.txt).CreationTime -lt (Get-Date).AddDays(-15)
+(Get-Item .\oui.txt).CreationTime -lt (Get-Date).AddDays(-15)
