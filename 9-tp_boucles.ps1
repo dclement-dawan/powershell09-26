76f1b0e81eb699021da3fc61945a017ddd22ba77
David CLEMENT <dclement@dawan.fr>
2026-09-22 09:56:33 +0200

    padding de chaine et whatif

--- 9-tp_boucles.ps1
+++ 9-tp_boucles.ps1
@@ -3,7 +3,16 @@
 
 
 
+
+
 # Q2: Suite à une erreur, on va renommer les fichiers ainsi :
 #    1.log devient  0001.log
 #           ...
 #  200.log devient  0200.log
+
+
+'1.log'.PadLeft(8, '0')
+'200.log'.PadLeft(8, '0')
+
+# exécution en dry-run
+Rename-Item -Path 1-intro.ps1 -NewName test.ps1 -WhatIf
