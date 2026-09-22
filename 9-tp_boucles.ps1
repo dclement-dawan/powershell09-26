165b9e54e443f3ff2e8972ff22ff1d1b9266348f
David CLEMENT <dclement@dawan.fr>
2026-09-22 10:31:54 +0200

    Correction tp boucles

--- 9-tp_boucles.ps1
+++ 9-tp_boucles.ps1
@@ -1,9 +1,27 @@
 # Q1: Créer 200 fichiers nommés 1.log à 200.log dans un dossier nommé "temporaire"
 
-
-
-
-
+New-Item temporaire -ItemType Directory
+# measure-command{
+for ($i = 1; $i -le 200; $i++) {
+    New-Item -Path temporaire -Name "$i.log"
+}
+#}
+# ou
+$i = 1
+while ($i -le 200) {
+    New-Item -Path temporaire -Name "$i.log"
+    $i++
+}
+# ou avec un intervalle
+foreach ($i in (1..200)) {
+    New-Item -Path temporaire -Name "$i.log"
+}
+# ou
+# Measure-Command{
+1..200 | ForEach-Object {
+    New-Item -Path temporaire -Name "$_.log"
+}
+# }
 
 # Q2: Suite à une erreur, on va renommer les fichiers ainsi :
 #    1.log devient  0001.log
@@ -16,3 +34,17 @@
 
 # exécution en dry-run
 Rename-Item -Path 1-intro.ps1 -NewName test.ps1 -WhatIf
+
+
+foreach ($file in (Get-ChildItem -Path .\temporaire -Filter *.log -File)) {
+    $NewName = $file.Name.PadLeft(8, '0')
+    Rename-Item -Path $file -NewName $NewName -WhatIf
+}
+# ou
+Get-ChildItem -Path .\temporaire -Filter *.log -File |
+    ForEach-Object {
+        $NewName = $_.Name.PadLeft(8, '0')
+        Rename-Item -Path $_ -NewName $NewName -WhatIf
+    }
+# ou
+ls temporaire\*.log | % {ren $_ $_.Name.PadLeft(8, '0')}
