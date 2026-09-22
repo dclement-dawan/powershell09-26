e8ad770bd3f3639353987963bd203805b3845491
David CLEMENT <dclement@dawan.fr>
2026-09-22 09:49:08 +0200

    TP sur les boucles

---
+++ 9-tp_boucles.ps1
@@ -0,0 +1,9 @@
+# Q1: Créer 200 fichiers nommés 1.log à 200.log dans un dossier nommé "temporaire"
+
+
+
+
+# Q2: Suite à une erreur, on va renommer les fichiers ainsi :
+#    1.log devient  0001.log
+#           ...
+#  200.log devient  0200.log
