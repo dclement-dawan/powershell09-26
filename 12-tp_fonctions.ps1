3a4b708d4c438c2ab868569df49e321c93b3203d
David CLEMENT <dclement@dawan.fr>
2026-09-22 11:45:32 +0200

    TP fonctions

---
+++ 12-tp_fonctions.ps1
@@ -0,0 +1,36 @@
+# 1- Créer une fonction Get-Min :
+#  - en entrée :
+#      $X obligatoire
+#      $Y obligatoire
+#  - en sortie
+#     retourne le plus petit entre $X et $Y
+
+
+
+# 2- Créer une fonction Get-Min2 :
+#  - en entrée :
+#      $X obligatoire de type int, compris entre -500 et 500
+#      $Y obligatoire de type int, compris entre -500 et 500
+#  - en sortie
+#     retourne le plus petit entre $X et $Y
+
+# [parameter(Mandatory)]
+# [ValidateRange(-500,500)]
+# [int]$x
+
+
+
+# 3- Créer une fonction Get-TabEntier, saisie par l'utilisateur avec un Read-Host, -1 pour sortir de la saisie
+# - en entrée : rien
+# - en sortie : un tableau d'entiers
+
+
+
+
+
+
+# 4- Créer une fonction Get-MinArray :
+#  - en entrée :
+#      $Tab un tableau d'entier [int[]]
+#  - en sortie
+#     retourne le plus petit du tableau
