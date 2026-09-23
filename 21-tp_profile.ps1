34bf1962af4e00062b2af22bc5fa14739495a85b
David CLEMENT <dclement@dawan.fr>
2026-09-23 15:10:44 +0200

    Correction tp profil

--- 21-tp_profile.ps1
+++ 21-tp_profile.ps1
@@ -6,13 +6,34 @@ Adresses IPv4 : 192.168.1.233, 192.168.56.1, 172.20.12.234
 
 V1 : Powershell
 Get-NetIpAddress
+#>
 
+# Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp, Manual | Select-Object IPAddress
+$tab = (Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp, Manual).IPAddress
+Write-Host 'Adresses IPv4 :' ($tab -join ', ')
+<#
 V2 : WMI
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration
 Get-CimInstance -Class Win32_NetworkAdapterConfiguration
+#>
+
+$tab = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration).IPAddress  |
+    Select-String -NotMatch ':' |
+    Select-String -NotMatch '^$'
+Write-Host  'Adresses IPv4 :' ($tab -join ', ')
+
+<#
 
 V3 :
 ipconfig.exe
+#>
+
+$tab = (ipconfig.exe | Select-String 'IPv4') -split ': ' |
+    Select-String -NotMatch 'IPv4'
+Write-Host  'Adresses IPv4 :' ($tab -join ', ')
+
+
+<#
 
 Mesurer la durée des différentes version avec Measure-Command
 =============================================
@@ -33,3 +54,23 @@ Get-Counter '\Processeur(_total)\% temps processeur'
 (Get-Counter '\Processeur(_total)\% temps processeur').CounterSamples.CookedValue
 #>
 
+
+
+(Get-Counter '\Processeur(_total)\% temps processeur').CounterSamples.CookedValue
+
+
+(Get-Counter -ListSet 'Mémoire').Counter
+
+$KBAvailable = (Get-Counter '\Mémoire\Kilo-octets disponibles').CounterSamples.CookedValue
+$KBTotal = (Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize
+
+$PercentUsed = ($KBTotal - $KBAvailable) / $KBTotal * 100
+Write-Host $PercentUsed
+
+# Arrondis :
+
+[int](59.58416070921954)             # arrondi monétaire à l'entier
+[Math]::Round(59.58416070921954)     # arrondi monétaire à l'entier
+[Math]::Round(59.58416070921954, 2)  # arrondi monétaire avec 2 chiffres après la virgule
+[Math]::Ceiling(59.58416070921954)   # arrondi plafond
+[Math]::Floor(59.58416070921954)     # arrondi plancher
--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,6 @@
 
 
-# Ajout de raccourci clavier
+# Ajout de raccourcis clavier
 
 # ctrl + d
 Set-PSReadLineKeyHandler -Chord 'ctrl+n' -ScriptBlock {
@@ -10,3 +10,76 @@ Set-PSReadLineKeyHandler -Chord 'ctrl+n' -ScriptBlock {
 }
 
 Set-PSReadLineKeyHandler -Chord 'ctrl+d' -Function DeleteCharOrExit
+
+
+$d1 = Measure-Command {
+$tab = (Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp, Manual).IPAddress
+Write-Host 'Adresses IPv4 :' ($tab -join ', ') -BackgroundColor Red
+}
+
+$d2 = Measure-Command {
+$tab = (Get-CimInstance -Class Win32_NetworkAdapterConfiguration).IPAddress  |
+    Select-String -NotMatch ':' |
+    Select-String -NotMatch '^$'
+Write-Host  'Adresses IPv4 :' ($tab -join ', ') -BackgroundColor Yellow
+}
+
+$d3 = Measure-Command {
+$tab = (ipconfig.exe | Select-String 'IPv4') -split ': ' |
+    Select-String -NotMatch 'IPv4'
+Write-Host  'Adresses IPv4 :' ($tab -join ', ') -BackgroundColor Green
+}
+
+Write-Host "Durée v1 $($d1.TotalMilliseconds)  v2 $($d2.TotalMilliseconds)  v3 $($d3.TotalMilliseconds)"
+
+
+
+
+Set-PSReadLineKeyHandler -Chord 'ctrl+t' -ScriptBlock {
+    $Proc = [int]((Get-Counter '\Processeur(_total)\% temps processeur').CounterSamples.CookedValue)
+    $KBAvailable = (Get-Counter '\Mémoire\Kilo-octets disponibles').CounterSamples.CookedValue
+    $KBTotal = (Get-CimInstance Win32_OperatingSystem).TotalVisibleMemorySize
+    $PercentUsed = [int](($KBTotal - $KBAvailable) / $KBTotal * 100)
+    Write-Host "CPU:${Proc}% Memory:${PercentUsed}%"
+    [Microsoft.Powershell.PSConsoleReadLine]::AcceptLine()
+}
+
+
+
+
+
+Set-PSReadlineKeyHandler -key 'Ctrl+o','Ctrl+O' `
+    -Description 'Open PowerShell session with user admin' `
+    -ScriptBlock {
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        if ($line.trim().length -ne 0) {
+            [Microsoft.Powershell.PSConsoleReadLine]::RevertLine()
+            [Microsoft.Powershell.PSConsoleReadLine]::Insert("Enter-PSSession -ComputerName $line -Credential admin")
+            [Microsoft.Powershell.PSConsoleReadLine]::AcceptLine()
+        }
+    }
+
+Set-PSReadlineKeyHandler -Key F1,"Shift+F1","Ctrl+F1" `
+    -BriefDescription Help `
+    -LongDescription 'Show command help' `
+    -ScriptBlock {
+        param($key, $arg)
+
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+        $command = $line.Split(' ')[0]
+
+        $option = ""
+        switch ($key.Modifiers) {
+            "Shift" {$option = "-Showwindow"}
+            "Control" {$option = "-Online"}
+        }
+
+        [Microsoft.Powershell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.Powershell.PSConsoleReadLine]::Insert("Get-Help $command $option")
+        [Microsoft.Powershell.PSConsoleReadLine]::AcceptLine()
+    }
